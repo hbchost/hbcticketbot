c710b9a4f2dc6e524d48fb1f301a1bb6c1a2d99c
schr1 <>
2021-08-08 22:25:29 +0000

    bot change

--- bot/main.py
+++ bot/main.py
@@ -1,20 +1,61 @@
 import discord
 import os
+from replit import db
+from cryptoaddress import EthereumAddress
 #import pynacl
 #import dnspython
 import server
-from discord.ext import commands
+#from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+#bot = commands.Bot(command_prefix="!")
+#TOKEN = os.getenv("DISCORD_TOKEN")
 
-@bot.event
+#@bot.event
+#async def on_ready():
+#    print(f"Logged in as {bot.user.name}({bot.user.id})")
+
+#@bot.command()
+#async def ping(ctx):
+#    await ctx.send("pong")
+
+#server.server()
+#bot.run(TOKEN)
+
+
+
+
+
+client = discord.Client()
+
+@client.event
+
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+  print("We have logged in as {0.user}".format(client))
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
-
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+      return
+    if isinstance(message.channel, discord.channel.DMChannel):
+      if message.author.id == 148038813048897536 and message.content.startswith("$list"):
+        keys = db.keys()
+        values = []
+        for discordid in keys:
+          eth_adr = db[discordid] 
+          values.append(eth_adr)
+        #remove duplicates
+        values = list(dict.fromkeys(values))
+        await message.author.send(values)
+        return
+      try:
+        eth_adr = EthereumAddress(message.content)
+        #print('The address "%s" is valid.' % str(eth_adr))
+        await message.author.send('**You will be able to redeem a HBC 90s Mint Pass.** The address **"%s"** is valid.' % str(eth_adr))
+        db[str(message.author.id)] = str(eth_adr)
+      except ValueError:
+        #print('Error: The address is invalid.')
+        await message.author.send('Error: The address is invalid.')
+      
 server.server()
-bot.run(TOKEN)
+TOKEN = os.getenv("DISCORD_TOKEN")
+client.run(TOKEN)
